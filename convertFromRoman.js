24cace95bcca5ebfd67e72bd907d0432d03a36a3
moblaze360 <victorvalenti@yahoo.co.uk>
2021-04-20 15:29:40 +0200

    Add files via upload

--- convertFromRoman.js
+++ convertFromRoman.js
@@ -1,4 +1,4 @@
-function convertRoman (str) {
+function convertFromRoman (str) {
     
         const roman = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
         let num = 0;
@@ -10,5 +10,5 @@ function convertRoman (str) {
         return num;
       };
   
-  export default convertRoman;
+  export default convertFromRoman;
   
