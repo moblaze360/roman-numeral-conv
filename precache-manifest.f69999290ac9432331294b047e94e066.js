72089b67302f5d5e8a7dc060c23da03df417bae3
moblaze360 <victorvalenti@yahoo.co.uk>
2021-04-20 15:52:05 +0200

    Add files via upload

---
+++ precache-manifest.f69999290ac9432331294b047e94e066.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72651cea12a33ed569f2a14f3010ebcf",
+    "url": "/roman-numeral-conv/index.html"
+  },
+  {
+    "revision": "0cf5320084a51a47b6c5",
+    "url": "/roman-numeral-conv/static/css/main.72415dc8.chunk.css"
+  },
+  {
+    "revision": "7088e6a6caa96bd84f7a",
+    "url": "/roman-numeral-conv/static/js/2.b484f2e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roman-numeral-conv/static/js/2.b484f2e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cf5320084a51a47b6c5",
+    "url": "/roman-numeral-conv/static/js/main.872b8aa7.chunk.js"
+  },
+  {
+    "revision": "0fb72ecab7d2e39e9f94",
+    "url": "/roman-numeral-conv/static/js/runtime-main.70cf302d.js"
+  },
+  {
+    "revision": "a8ce62e7629d4ad14709bd01e724db3d",
+    "url": "/roman-numeral-conv/static/media/MouseMemoirs-Regular.a8ce62e7.ttf"
+  }
+]);
